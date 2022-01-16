b444220597c1db15ccde7a7ea715358959d6a5c3
Jonny10Pum <jonathangardunodelacruz07@gmail.com>
2022-01-15 23:16:10 -0600

    Cambios en propiedades de tabla carreras

---
+++ database/migrations/2022_01_16_050041_cambiar_propiedades_to_carreras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CambiarPropiedadesToCarrerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('carreras', function (Blueprint $table) {
+            //
+            $table->string('carrera', 50)->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('carreras', function (Blueprint $table) {
+            //
+            $table->string('carrera', 50)->change();
+
+        });
+    }
+}
